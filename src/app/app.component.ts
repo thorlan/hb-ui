fb051b5e9bc866845a805ff6832e02c6bcf8b888
pakomrs <mmoraes@tbg.com.br>
2020-05-14 21:45:59 -0400

    Co-authored-by: Thiago <thiagorlandini@hotmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'home-broken-ui';
+  
+  public lineChartData: ChartDataSets[] = [
+    { data: [], label: 'Gráfico comum'  ,fill: false },
+    { data: [], label: 'EMA 9'  ,fill: false},
+    { data: [], label: 'EMA 12' ,fill: false },
+    { data: [], label: 'EMA 26' ,fill: false }
+  ];
+  public lineChartLabels: Label[] = [];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+  
+  
   ngOnInit() {
-    fetch('https://hb-abc.herokuapp.com/abc-hb/api/grafico').then(response =>response.json()).then(json => console.log(json))
+    fetch('http://localhost:8080/abc-hb/api/grafico')
+      .then(response =>response.json())
+      .then(json => {
+        
+         json[0].dia.forEach(element => {
+          this.lineChartLabels.push(element.date);
+          this.lineChartData[0].data.push(element.close);
+        });
+
+         json[1].dia.forEach(element => {
+          this.lineChartData[1].data.push(element.close);
+        });
+
+        json[2].dia.forEach(element => {
+          this.lineChartData[2].data.push(element.close);
+        });
+
+        json[3].dia.forEach(element => {
+          this.lineChartData[3].data.push(element.close);
+        });
+     
+      });
+   //fetch('https://hb-abc.herokuapp.com/abc-hb/api/grafico').then(response =>response.json()).then(json => console.log(json))
   };
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { ChartsModule } from 'ng2-charts';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 
-import { ChartsModule } from 'ng2-charts';
 import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
 import { MyDoughnutChartComponent } from './my-doughnut-chart/my-doughnut-chart.component';
 import { MyRadarChartComponent } from './my-radar-chart/my-radar-chart.component';
 import { MyPieChartComponent } from './my-pie-chart/my-pie-chart.component';
 
 import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'bar-chart', component: MyBarChartComponent},
+  {path: 'doughnut-chart', component: MyDoughnutChartComponent},
+  {path: 'radar-chart', component: MyRadarChartComponent},
+  {path: 'pie-chart', component: MyPieChartComponent},
+  {path: '**', component: MyBarChartComponent }
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +30,8 @@ import { Routes, RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    ChartsModule 
+    RouterModule.forRoot(routes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
